7981ed8914da2a77160e52b21c3f4481ed026487
Tristan Bennett <tristanbennett@tristanova.local>
2019-01-02 16:00:47 -0600

    added click event and function to app.js file

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 //test files are connected properly 
 console.log("Testing, 1, 2, 3!");
+
+// add click event
+
+$("#add-task-button").click(function() {
+    let task = $("#add-task-input").val();
+    if (task == "") {
+        alert("Task cannot be blank.");
+    } else {
+        $("#to-do-list").append(`<li>${task}</li>`);
+        $("#add-task-input").val("");
+    }
+});
